ca51c340c6bcb74b1698a4023abba6d9fe6f31eb
Jhordy Gavinchu <dev@jhordyess.com>
2023-05-17 18:35:48 +0000

    Used spring initializer ⚡ - Maven project - Java 17 - Spring Boot 3.0.6 - Packaging Jar - Dependencies: Spring Web, Lombok

---
+++ src/main/java/com/jhordyess/javaapifake/JavaApiFakeApplication.java
@@ -0,0 +1,13 @@
+package com.jhordyess.javaapifake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiFakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiFakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhordyess/javaapifake/JavaApiFakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhordyess.javaapifake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiFakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
